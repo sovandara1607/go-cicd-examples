208bc8983c0d03df994e58e8b82d03c217aad757
sovandara1607 <srith3@paragoniu.edu.kh>
2026-05-22 16:03:58 +0700

    update Go module

--- main.go
+++ main.go
@@ -11,9 +11,7 @@ func main() {
 	})
 	fmt.Println("Server listening on :8080")
 	http.ListenAndServe(":8080", nil)
-}
 
-func checkHealth(){
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Health check passed!")
 	})
