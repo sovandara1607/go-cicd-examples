fbcaac5e378a1de5ff52dc26cc2401b5edeba450
sovandara1607 <srith3@paragoniu.edu.kh>
2026-05-23 00:21:43 +0700

    Add Go CI Docker workflow

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
-
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 )
 
-func main (){
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
